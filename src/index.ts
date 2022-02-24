3d292639ae1794d9d4c595ce6f81a8adec24b281
coofzilla <jeric614@yahoo.com>
2022-02-24 13:20:56 +0900

    additional boilerplate f/mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
 import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { mongoURI } from "./config/dev";
 
 const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log("CONNECTED TO MONGODB");
+  } catch (err) {
+    console.log("ERROR CONNECTING", err);
+  }
+})();
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 app.listen(4000, () => {
   console.log("LISTENING ON 4000");
